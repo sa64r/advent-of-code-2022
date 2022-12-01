cdd2d9ce5d4fec2c7079f8e50999ec0661808973
Sagar Shah <sa64r.shah@gmail.com>
2022-12-01 09:20:57 +0000

    day1 advent of code

---
+++ day1/index.ts
@@ -0,0 +1,51 @@
+import { parseLines } from '../utils/parse';
+import { solve } from '../utils/solve';
+
+function part1(_input: string[]) {
+
+  let largestCalorieCount = 0;
+
+  let currCount = 0;
+
+  for(let i = 0; i < _input.length; i++) {
+
+    if(_input[i] === '') {
+      if(currCount > largestCalorieCount) {
+        largestCalorieCount = currCount;
+      }
+      currCount = 0;
+    } else{
+      currCount += parseInt(_input[i] as string, 0);
+    }
+
+  }
+
+
+  return largestCalorieCount.toString();
+}
+
+function part2(_input: string[]) {
+
+  let elvesCalorieCount = [];
+
+  let currCount = 0;
+
+  for(let i = 0; i < _input.length; i++) {
+
+    if(_input[i] === '') {
+      elvesCalorieCount.push(currCount);
+      currCount = 0;
+    } else{
+      currCount += parseInt(_input[i] as string, 0);
+    }
+
+  }
+  elvesCalorieCount.sort((a, b) => b - a);
+
+  let topThree = elvesCalorieCount.slice(0, 3).reduce((a, b) => a + b, 0);
+
+
+  return topThree.toString();
+}
+
+solve({ part1, part2, parser: parseLines });
